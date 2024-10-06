2d70cb0e43ea7fa2ecc380cd5d91adc4eae26ec8
soura07 <nitsouradeep2022@gmail.com>
2024-10-07 01:32:06 +0530

    updated

---
+++ src/components/LineChart/LineChart.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import Chart from 'react-google-charts';
+
+const LineChart = ({ historicalData }) => {
+  const [data, setData] = useState([['Date', 'Prices']]);
+
+  useEffect(() => {
+    let dataCopy = [['Date', 'Prices']];
+    if (historicalData?.prices) {
+      historicalData.prices.forEach(item => {
+        dataCopy.push([new Date(item[0]).toLocaleDateString(), item[1]]);
+      });
+      setData(dataCopy);
+    }
+  }, [historicalData]);
+
+  return (
+    <Chart
+      chartType="LineChart"
+      data={data}
+      width="100%"
+      height="400px"
+      legendToggle
+    />
+  );
+};
+
+export default LineChart;
